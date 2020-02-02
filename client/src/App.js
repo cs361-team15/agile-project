06a77a14c0731512809c6c4b9e3404ec7cd9435c
JohnNWarila <johnwarila@gmail.com>
2020-02-01 16:10:16 -0800

    Added react-router-dom for routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router } from "react-router-dom"
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <h1>STOCK APP</h1>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <h1>STOCK APP</h1>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
