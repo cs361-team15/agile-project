d34d6e94db10a40c8ebed824bc005598f719a1fa
aDerringer <47407127+aDerringer@users.noreply.github.com>
2020-02-10 00:02:43 -0800

    stock and portfolio crud added

--- dao_services/DBminion.py
+++ dao_services/DBminion.py
@@ -5,7 +5,7 @@ from datetime import date, datetime, timedelta
 
 class DBminion:
 
-    def __init__(self, u, p, h, db, parent=None):
+    def __init__(self, u, p, h, db):
 
         self.DBconfig = {
             'user': u,
@@ -49,7 +49,7 @@ class DBminion:
         return result
 
 
-    def add_user(self, email, password, first_name, last_name):
+    def insertUser(self, email, password, first_name, last_name):
         user_insert = ("INSERT INTO user "
                     "(email, password, first_name, last_name) "
                     "VALUES (%s, %s, %s, %s) ;")
@@ -58,29 +58,27 @@ class DBminion:
         self._DB_CUD(user_insert, user_data)
 
     
-    def remove_user(self, email, password):
+    def removeUser(self, email, password):
         user_remove = ("DELETE FROM user "
                         "WHERE email = %s AND password = %s ;")
 
         self._DB_CUD(user_remove, (email, password))
 
 
-    def get_user(self, email):
+    def selectUser(self, email):
         user_query = ("SELECT first_name, last_name FROM user "
                         "WHERE email = %s ;")
         
         return self._DB_R(user_query, (email,))
 
 
-    def get_all_users(self):
+    def selectAllUsers(self):
         user_query = ("SELECT * FROM user ;")
 
         return self._DB_R(user_query, None)
         
 
-    def add_portfolio(self, name, email):
-        #activation_date = datetime.today().strftime('%Y-%m-%d')
-        #print(activation_date)
+    def insertPortfolio(self, name, email):
         portfolio_insert = ("INSERT INTO portfolio "
                             "(name, activation_date, user_id) SELECT %s, %s, user.id "
                             "FROM user WHERE email = %s ;")
@@ -88,18 +86,60 @@ class DBminion:
         self._DB_CUD(portfolio_insert, (name, datetime.today(), email))
 
     
-    # def remove_portfolio(self, name, email):
-    #     portfolio_remove = ("DELETE")
+    def removePortfolio(self, name, email):
+        portfolio_remove = ("DELETE portfolio FROM portfolio "
+                                "INNER JOIN user ON portfolio.user_id = user.id "
+                                "WHERE portfolio.name = %s AND user.email = %s ;")
 
-    def get_all_portfolios(self):
+        self._DB_CUD(portfolio_remove, (name, email))
+
+
+    def selectPortfolio(self, name, email):
+        portfolio_query = ("SELECT activation_date, total_value, balance FROM portfolio "
+                        "INNER JOIN user ON portfolio.user_id = user.id "
+                        "WHERE portfolio.name = %s AND user.email = %s ;")
+        
+        return self._DB_R(portfolio_query, (name, email))
+
+
+    def selectAllUserPortfolios(self, email):
+        portfolio_query = ("SELECT name, activation_date, total_value, balance FROM portfolio "
+                        "INNER JOIN user ON portfolio.user_id = user.id "
+                        "WHERE user.email = %s ;")
+        
+        return self._DB_R(portfolio_query, (email,))
+
+
+    def selectAllPortfolios(self):
         portfolio_query = ("SELECT * FROM portfolio ;")
 
         return self._DB_R(portfolio_query, None)
 
 
+    def insertStock(self, ticker, name, open_price, current_price, current_volume, market_cap, fifty_two_week_high, fifty_two_week_low):
+        stock_insert = ("INSERT INTO stock "
+                    "(ticker, name, open_price, current_price, current_volume, market_cap, fifty_two_week_high, fifty_two_week_low) "
+                    "VALUES (%s, %s, %s, %s, %s, %s, %s, %s) ;")
+        stock_data = (ticker, name, open_price, current_price, current_volume, market_cap, fifty_two_week_high, fifty_two_week_low)
 
+        self._DB_CUD(stock_insert, stock_data)
+
+
+    def removeStock(self, ticker):
+        stock_remove = ("DELETE FROM stock "
+                        "WHERE ticker = %s ;")
+
+        self._DB_CUD(stock_remove, (ticker, ))
+        
+    
+    def selectAllStocks(self):
+        stock_query = ("SELECT * FROM stock ;")
+
+        return self._DB_R(stock_query, None)
 
 
 minion = DBminion('derringa', 'america', '10.0.0.183', 'stock_app')
-minion.add_portfolio("sports", 'robert@gmail.com')
-print(minion.get_all_portfolios())
+
+# minion.insertStock("AMZN", "Amazon.com Inc", "5.5", "7.79", "5", "15.44", "13.10", "4.99")
+
+print(minion.selectAllStocks())
